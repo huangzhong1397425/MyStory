dcd5b1c409ae87395b8c163ba6ce67fea461e4a8
huangzhong1397425 <huangzhong1397425@163.com>
2015-04-01 22:01:53 +0800

    create project
    create new project

---
+++ MyStory/MyStory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyStory
+//
+//  Created by HZ on 15/4/1.
+//  Copyright (c) 2015年 HuangZhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
